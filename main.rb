a99c4525c8e29ddb0d05eb92b36f30dbd68d9166
Dandush03 <d.laloush@outlook.com>
2020-02-12 15:21:15 -0400

    Fixing Sticklers Error

--- main.rb
+++ main.rb
@@ -1,14 +1,13 @@
 def bubble_sort(item)
-
-  (1...(item.length())).each { 
+  (1..(item.length-1)).each { 
     |n|
-    (0...(item.length()-1)).each {
+    (0..(item.length-2)).each {
       |k|
       if(item[k].to_i > item[k+1].to_i) then
         item[k],item[k+1] = item[k+1],item[k]
       end
     }
-  }	
+  }
 
   return item.inspect
 end
