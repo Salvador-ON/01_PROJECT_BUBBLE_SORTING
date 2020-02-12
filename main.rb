ee8960ad746f84313d34347df0522e5433487551
Salvador <salvador.on@outlook.com>
2020-02-12 16:57:29 -0300

    fix l13  lint errors

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ end
 def bubble_sort_by(item)
   (1..(item.length - 1)).each do
     (0..(item.length - 2)).each do |k|
-      item[k],item[k+1] = item[k+1],item[k] if item[k].length > item[k + 1].length 
+      item[k], item[k+1] = item[k + 1], item[k] if item[k].length > item[k + 1].length 
     end
   end
 
