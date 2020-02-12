087207ca4340b2484d74bd2b83be1096fe729954
Salvador <salvador.on@outlook.com>
2020-02-12 18:19:19 -0300

    add raise

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 def bubble_sort(item)
+  raise RuntimeError, 'empty argument' if item[0].nil?
   (1..(item.length - 1)).each do
     (0..(item.length - 2)).each do |k|
       item[k], item[k + 1] = item[k + 1], item[k] if item[k].to_i > item[k + 1].to_i
@@ -8,6 +9,7 @@ def bubble_sort(item)
 end
 
 def bubble_sort_by(item)
+  raise RuntimeError, 'empty argument' if item[0].nil?
   (1..(item.length - 1)).each do
     (0..(item.length - 2)).each do |k|
       item[k], item[k + 1] = item[k + 1], item[k] if item[k].length > item[k + 1].length
@@ -18,4 +20,4 @@ end
 
 puts bubble_sort(['3', '2', '1', '6', '5', '2', '10', '0', '-3'])
 
-puts bubble_sort_by(%w[length hi hello hey a abc])
+puts bubble_sort_by(%w[])
