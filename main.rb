8611a5e53adaf7cb308933b47c8d8ee035cdf56c
Salvador <salvador.on@outlook.com>
2020-02-13 11:57:38 -0300

    add raise block

--- main.rb
+++ main.rb
@@ -10,16 +10,23 @@ def bubble_sort(item)
 end
 
 def bubble_sort_by(item)
-  raise ArgumentError if item[0].nil?
+  raise 'No block_given' unless block_given?
 
-  (1..(item.length - 1)).each do
+  sorting = false
+  until sorting == true
+    x = 0
     (0..(item.length - 2)).each do |k|
-      item[k], item[k + 1] = item[k + 1], item[k] if item[k].length > item[k + 1].length
+      if yield(item[k], item[k + 1]).positive?
+        item[k], item[k + 1] = item[k + 1], item[k]
+        x += 1
+      end
     end
+    sorting = true if x.zero?
   end
-  item = item
+  item
 end
 
 puts bubble_sort(['3', '2', '1', '6', '5', '2', '10', '0', '-3'])
 
-puts bubble_sort_by(%w[hello abc ki a helloworld])
+puts bubble_sort_by(%w[hello abc ki a helloworld]) { |left, right| left.length - right.length }
+puts bubble_sort_by(%w[hello abc ki a helloworld]) { |left, right| right.length - left.length }
